e88f2059beb7d94807ee9717c505e4f5ec2a91b0
Marcos Caceres <marcos.caceres@wacapps.net>
2012-10-25 16:39:22 +0100

    Added a bit of security : Check that the requests originates from Github

--- issuechange.php
+++ issuechange.php
@@ -1,4 +1,10 @@
 <?
-require_once 'issues.php';
-fetch_issues('open');
-fetch_issues('closed');
+//Check that the requests originates from Github 
+$svr = $_SERVER['REMOTE_ADDR']; 
+if($svr == "207.97.227.253" || $svr == "50.57.128.197" || $svr == "108.171.174.178"){
+	require_once 'issues.php';
+	fetch_issues('open');
+	fetch_issues('closed');
+}else{
+	//HttpResponse::status(401);
+}
