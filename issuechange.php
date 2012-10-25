447bfe2b852d92342394e1fa91ff8b6596b05d12
Marcos Caceres <marcosscaceres@gmail.com>
2012-10-25 19:25:43 +0100

    cleaned up comment, ignoring a google verification file.

--- issuechange.php
+++ issuechange.php
@@ -1,6 +1,7 @@
 <?
 //Check that the requests originates from Github 
-$svr = $_SERVER['REMOTE_ADDR']; 
+//IP addresses are derived from Github's Web Hook.  
+$svr = $_SERVER['REMOTE_ADDR'];
 if($svr == "207.97.227.253" || $svr == "50.57.128.197" || $svr == "108.171.174.178"){
 	require_once 'issues.php';
 	fetch_issues('open');
