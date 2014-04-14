789a387245654dbe11a90617ec782e9064080f9a
Marcos Caceres <marcos@marcosc.com>
2014-04-14 11:59:11 -0400

    No longer tracking ri-usecases issues.

--- issues-opt.php
+++ issues-opt.php
@@ -3,7 +3,6 @@
 	$config['openCacheFile'] = "cache/open-issues.cache";
 	$config['closedCacheFile'] = "cache/closed-issues.cache";
 	$config['repos'] = array(
-			'https://api.github.com/repos/ResponsiveImagesCG/picture-element/issues',
-			'https://api.github.com/repos/ResponsiveImagesCG/ri-usecases/issues'
+			'https://api.github.com/repos/ResponsiveImagesCG/picture-element/issues'
 		);
 ?>
